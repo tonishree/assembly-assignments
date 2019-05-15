15945132577c05ff0d011b9bb4fc474ed1ed5d82
tonishree <38374920+tonishree@users.noreply.github.com>
2019-05-15 17:53:00 +0700

    Update md5.cpp

--- md5.cpp
+++ md5.cpp
@@ -1,5 +1,4 @@
 /* MD5
-
 /* interface header */
 #include "md5.h"
 
@@ -33,11 +32,11 @@ inline MD5::uint4 MD5::F(uint4 x, uint4 y, uint4 z) {
 }
 
 inline MD5::uint4 MD5::G(uint4 x, uint4 y, uint4 z) {
-	return x & z | y & ~z;
+	return ((x | y) & z) | (x & y);
 }
 
 inline MD5::uint4 MD5::H(uint4 x, uint4 y, uint4 z) {
-	return x ^ y^ z;
+	return x ^ y ^ z;
 }
 
 inline MD5::uint4 MD5::I(uint4 x, uint4 y, uint4 z) {
@@ -65,11 +64,21 @@ inline void MD5::FF(uint4 & a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint
 }
 
 inline void MD5::GG(uint4 & a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + G(b, c, d) + x + ac, s) + b;
+	//a = rotate_left(a + G(b, c, d) + x + ac, s) + b;
+	a = a + G(b, c, d) + x + 0x5A826999;
+	uint4 f = a;
+	f = rotate_right(f, s);
+	a = rotate_left(a, ac);
+	a = f | a;
 }
 
 inline void MD5::HH(uint4 & a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
-	a = rotate_left(a + H(b, c, d) + x + ac, s) + b;
+	//a = rotate_left(a + H(b, c, d) + x + ac, s) + b;
+	a = a + H(b, c, d) + x + 0x6ED9FBA1;
+	uint4 f = a;
+	f = rotate_right(f, s);
+	a = rotate_left(a, ac);
+	a = f | a;
 }
 
 inline void MD5::II(uint4 & a, uint4 b, uint4 c, uint4 d, uint4 x, uint4 s, uint4 ac) {
@@ -192,63 +201,103 @@ void MD5::transform(const uint1 block[blocksize])
 	FF(b, c, d, a, x[15], 0x13, 0xD); /* 16 */
 
 	/* Round 2 */
-	GG(a, b, c, d, x[1], S21, 0xf61e2562); /* 17 */
-	GG(d, a, b, c, x[6], S22, 0xc040b340); /* 18 */
-	GG(c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
-	GG(b, c, d, a, x[0], S24, 0xe9b6c7aa); /* 20 */
-	GG(a, b, c, d, x[5], S21, 0xd62f105d); /* 21 */
-	GG(d, a, b, c, x[10], S22, 0x2441453); /* 22 */
-	GG(c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
-	GG(b, c, d, a, x[4], S24, 0xe7d3fbc8); /* 24 */
-	GG(a, b, c, d, x[9], S21, 0x21e1cde6); /* 25 */
-	GG(d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
-	GG(c, d, a, b, x[3], S23, 0xf4d50d87); /* 27 */
-	GG(b, c, d, a, x[8], S24, 0x455a14ed); /* 28 */
-	GG(a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
-	GG(d, a, b, c, x[2], S22, 0xfcefa3f8); /* 30 */
-	GG(c, d, a, b, x[7], S23, 0x676f02d9); /* 31 */
-	GG(b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
+	//GG(a, b, c, d, x[1], S21, 0xf61e2562);
+	GG(a, b, c, d, x[0], 0x1D, 0x3); /* 17 */
+	//GG(d, a, b, c, x[6], S22, 0xc040b340);
+	GG(d, a, b, c, x[4], 0x1B, 0x5); /* 18 */
+	GG(c, a, b, d, x[8], 0x17, 0x9); /* 19 */
+	GG(b, c, a, d, x[12], 0x13, 0xD); /* 20 */
+
+	GG(a, b, c, d, x[1], 0x1D, 0x3); /* 21 */
+	GG(d, a, b, c, x[5], 0x1B, 0x5); /* 22 */
+	GG(c, a, b, d, x[9], 0x17, 0x9); /* 23 */
+	GG(b, c, a, d, x[13], 0x13, 0xD); /* 24 */
+
+	GG(a, b, c, d, x[2], 0x1D, 0x3); /* 25 */
+	GG(d, a, b, c, x[6], 0x1B, 0x5); /* 26 */
+	GG(c, a, b, d, x[10], 0x17, 0x9); /* 27 */
+	GG(b, c, a, d, x[14], 0x13, 0xD); /* 28 */
+
+	GG(a, b, c, d, x[3], 0x1D, 0x3); /* 29 */
+	GG(d, a, b, c, x[7], 0x1B, 0x5); /* 30 */
+	GG(c, a, b, d, x[11], 0x17, 0x9); /* 31 */
+	GG(b, c, a, d, x[15], 0x13, 0xD); /* 32 */
+
+	//GG(c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
+	//GG(b, c, d, a, x[0], S24, 0xe9b6c7aa); /* 20 */
+	//GG(a, b, c, d, x[5], S21, 0xd62f105d); /* 21 */
+	//GG(d, a, b, c, x[10], S22, 0x2441453); /* 22 */
+	//GG(c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
+	//GG(b, c, d, a, x[4], S24, 0xe7d3fbc8); /* 24 */
+	//GG(a, b, c, d, x[9], S21, 0x21e1cde6); /* 25 */
+	//GG(d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
+	//GG(c, d, a, b, x[3], S23, 0xf4d50d87); /* 27 */
+	//GG(b, c, d, a, x[8], S24, 0x455a14ed); /* 28 */
+	//GG(a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
+	//GG(d, a, b, c, x[2], S22, 0xfcefa3f8); /* 30 */
+	//GG(c, d, a, b, x[7], S23, 0x676f02d9); /* 31 */
+	//GG(b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
 
 	/* Round 3 */
-	HH(a, b, c, d, x[5], S31, 0xfffa3942); /* 33 */
-	HH(d, a, b, c, x[8], S32, 0x8771f681); /* 34 */
-	HH(c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
-	HH(b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
-	HH(a, b, c, d, x[1], S31, 0xa4beea44); /* 37 */
-	HH(d, a, b, c, x[4], S32, 0x4bdecfa9); /* 38 */
-	HH(c, d, a, b, x[7], S33, 0xf6bb4b60); /* 39 */
-	HH(b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
-	HH(a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
-	HH(d, a, b, c, x[0], S32, 0xeaa127fa); /* 42 */
-	HH(c, d, a, b, x[3], S33, 0xd4ef3085); /* 43 */
-	HH(b, c, d, a, x[6], S34, 0x4881d05); /* 44 */
-	HH(a, b, c, d, x[9], S31, 0xd9d4d039); /* 45 */
-	HH(d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
-	HH(c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
-	HH(b, c, d, a, x[2], S34, 0xc4ac5665); /* 48 */
+	//HH(a, b, c, d, x[5], S31, 0xfffa3942); /* 33 */
+	//HH(d, a, b, c, x[8], S32, 0x8771f681); /* 34 */
+	//HH(c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
+	//HH(b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
+	//HH(a, b, c, d, x[1], S31, 0xa4beea44); /* 37 */
+	//HH(d, a, b, c, x[4], S32, 0x4bdecfa9); /* 38 */
+	//HH(c, d, a, b, x[7], S33, 0xf6bb4b60); /* 39 */
+	//HH(b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
+	//HH(a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
+	//HH(d, a, b, c, x[0], S32, 0xeaa127fa); /* 42 */
+	//HH(c, d, a, b, x[3], S33, 0xd4ef3085); /* 43 */
+	//HH(b, c, d, a, x[6], S34, 0x4881d05); /* 44 */
+	//HH(a, b, c, d, x[9], S31, 0xd9d4d039); /* 45 */
+	//HH(d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
+	//HH(c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
+	//HH(b, c, d, a, x[2], S34, 0xc4ac5665); /* 48 */
+
+	HH(a, b, c, d, x[0], 0x1D, 0x3); /* 33 */
+	HH(d, a, b, c, x[8], 0x17, 0x9); /* 34 */
+	HH(c, d, a, b, x[4], 0x15, 0xB); /* 35 */
+	HH(b, c, d, a, x[12], 0x11, 0xF); /* 36 */
+
+	HH(a, b, c, d, x[2], 0x1D, 0x3); /* 37 */
+	HH(d, a, b, c, x[10], 0x17, 0x9); /* 38 */
+	HH(c, d, a, b, x[6], 0x15, 0xB); /* 39 */
+	HH(b, c, d, a, x[14], 0x11, 0xF); /* 40 */
+
+	HH(a, b, c, d, x[1], 0x1D, 0x3); /* 41 */
+	HH(d, a, b, c, x[9], 0x17, 0x9); /* 42 */
+	HH(c, d, a, b, x[5], 0x15, 0xB); /* 43 */
+	HH(b, c, d, a, x[13], 0x11, 0xF); /* 44 */
+
+	HH(a, b, c, d, x[3], 0x1D, 0x3); /* 45 */
+	HH(d, a, b, c, x[11], 0x17, 0x9); /* 46 */
+	HH(c, d, a, b, x[7], 0x15, 0xB); /* 47 */
+	HH(b, c, d, a, x[15], 0x11, 0xF); /* 48 */
 
 	/* Round 4 */
-	II(a, b, c, d, x[0], S41, 0xf4292244); /* 49 */
-	II(d, a, b, c, x[7], S42, 0x432aff97); /* 50 */
-	II(c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
-	II(b, c, d, a, x[5], S44, 0xfc93a039); /* 52 */
-	II(a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
-	II(d, a, b, c, x[3], S42, 0x8f0ccc92); /* 54 */
-	II(c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
-	II(b, c, d, a, x[1], S44, 0x85845dd1); /* 56 */
-	II(a, b, c, d, x[8], S41, 0x6fa87e4f); /* 57 */
-	II(d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
-	II(c, d, a, b, x[6], S43, 0xa3014314); /* 59 */
-	II(b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
-	II(a, b, c, d, x[4], S41, 0xf7537e82); /* 61 */
-	II(d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
-	II(c, d, a, b, x[2], S43, 0x2ad7d2bb); /* 63 */
-	II(b, c, d, a, x[9], S44, 0xeb86d391); /* 64 */
+	//II(a, b, c, d, x[0], S41, 0xf4292244); /* 49 */
+	//II(d, a, b, c, x[7], S42, 0x432aff97); /* 50 */
+	//II(c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
+	//II(b, c, d, a, x[5], S44, 0xfc93a039); /* 52 */
+	//II(a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
+	//II(d, a, b, c, x[3], S42, 0x8f0ccc92); /* 54 */
+	//II(c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
+	//II(b, c, d, a, x[1], S44, 0x85845dd1); /* 56 */
+	//II(a, b, c, d, x[8], S41, 0x6fa87e4f); /* 57 */
+	//II(d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
+	//II(c, d, a, b, x[6], S43, 0xa3014314); /* 59 */
+	//II(b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
+	//II(a, b, c, d, x[4], S41, 0xf7537e82); /* 61 */
+	//II(d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
+	//II(c, d, a, b, x[2], S43, 0x2ad7d2bb); /* 63 */
+	//II(b, c, d, a, x[9], S44, 0xeb86d391); /* 64 */
 
-	state[0] += a;
+	//state[0] += a;
 	state[1] += b;
-	state[2] += c;
-	state[3] += d;
+	//state[2] += c;
+	//state[3] += d;
 
 	// Zeroize sensitive information.
 	memset(x, 0, sizeof x);
@@ -308,9 +357,9 @@ void MD5::update(const char input[], size_type length)
 MD5& MD5::finalize()
 {
 	static unsigned char padding[64] = {
-	  0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+		0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 	};
 
 	if (!finalized) {
