b9a2bbbe315eb09cdf9e1026ab682e92f8757c03
tonishree <38374920+tonishree@users.noreply.github.com>
2019-05-15 14:51:03 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include "md5.h"
+using namespace std;
+
+using std::cout; using std::endl;
+
+int main(int argc, char* argv[])
+{
+	cout << "md5 of : " << md5("5P1KQ") << endl;
+	return 0;
+}
+
+void CustomHash(string& p1)
+{
+	//init hash
+	string a = "67452301"; // c = reverse(a)
+	string b = "EFCDAB89"; // d = reverse(b)
+	
+	//update hash
+
+
+
+	//finalize hash
+
+	//-> hash 1
+
+	//update hash
+
+	//update hash ->
+
+	//-> transform hash
+
+	//-> hash 2
+
+	//finalize hash
+	//-> hash 1
+
+}
+
+//update Hash
